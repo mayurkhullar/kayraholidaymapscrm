863340ab563302cdaa0af36637b88e3246bd6a67
mayurkhullar <gmservices1954@gmail.com>
2026-03-18 13:14:49 +0530

    Setup Firebase infrastructure for Flutter app

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -11,18 +11,12 @@ class DefaultFirebaseOptions {
 
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions has not been configured for web. '
-        'Run `flutterfire configure` to generate lib/firebase_options.dart.',
-      );
+      return web;
     }
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions has not been configured for Android. '
-          'Run `flutterfire configure` to generate lib/firebase_options.dart.',
-        );
+        return android;
       case TargetPlatform.iOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions has not been configured for iOS. '
@@ -50,4 +44,22 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCeo59act9M4WL8leXx_L5ACs9M_IT8FMQ',
+    appId: '1:1090709913179:web:036806f316e4ec77f2097e',
+    messagingSenderId: '1090709913179',
+    projectId: 'kayracrm2026',
+    authDomain: 'kayracrm2026.firebaseapp.com',
+    storageBucket: 'kayracrm2026.firebasestorage.app',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyA5jmv-ZC-8C2rF97hMs4pCbK_OttOl-zY',
+    appId: '1:1090709913179:android:1357f3b10a79cf81f2097e',
+    messagingSenderId: '1090709913179',
+    projectId: 'kayracrm2026',
+    storageBucket: 'kayracrm2026.firebasestorage.app',
+  );
+
 }
