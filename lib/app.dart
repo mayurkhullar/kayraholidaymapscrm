6706eb8ddf78c116bc380f51facb3573d3bc8c43
mayurkhullar <gmservices1954@gmail.com>
2026-03-20 12:54:06 +0530

    fix: restore normal app routing and set leads as temporary initial route

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'core/constants/app_environment.dart';
 import 'core/theme/app_theme.dart';
-import 'dev/dev_test_screen.dart';
+import 'core/utils/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,10 +12,11 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppEnvironment.appName,
-      home: const DevTestScreen(),
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
+      initialRoute: AppRouter.leadsRoute,
+      onGenerateRoute: AppRouter.onGenerateRoute,
     );
   }
 }
